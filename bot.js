2288d54f7a904e21fc296624053f26c047669d36
ShonShonChan <bothb7963@gmail.com>
2025-09-07 02:39:01 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
@@ -33,3 +33,4 @@ bot.on('message', (msg) => {
 });
 
 console.log('✅ Bot is running...');
+
